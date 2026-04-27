d4eabd6f64848e3f16153ec15fe3c389623d767a
Russel kouame <kr23cj@gmail.com>
2026-04-27 20:13:09 +0000

    Base_HTML/CSS_complet_new_js

---
+++ script.js
@@ -0,0 +1,11 @@
+// variables nécessaires
+var h,mn,s,ms,t,btn_stop,btn_start,sp;
+
+/* 
+    Initialisation des variables au chagement complet de la page.
+
+*/
+window.addEventListener('load', function(){
+    sp = document.getElementsByTagName('span');
+    btn_start = document.getElementById('start')
+})
